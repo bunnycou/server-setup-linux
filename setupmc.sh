c5455c05da2362b3af85c017db677be401af6e3a
Noah Cou <noahjcou@gmail.com>
2020-08-28 10:01:23 -0400

    Added restartability to minecraft servers

--- setupmc.sh
+++ setupmc.sh
@@ -17,12 +17,24 @@ then
                 echo "Updating Paper-$ver . . ."
                 rm paper-$ver/server-*.jar
                 curl https://papermc.io/api/v1/paper/$2/latest/download -o paper-$ver/server-$2.jar
+                echo "while true; do" > paper-$ver/start
+                echo "java -Xmx8G -jar server-$2.jar" >> paper-$ver/start
+                echo "echo Restarting in 15 seconds..." >> paper-$ver/start
+                echo "sleep 15" >> paper-$ver/start
+                echo "done" >> paper-$ver/start
+                chmod +x paper-$ver/start
             ;;
 
             waterfall|w)
                 echo "updating Waterfall!"
                 rm waterfall-$ver/waterfall-*.jar
                 curl https://papermc.io/api/v1/waterfall/$2/latest/download -0 waterfall-$ver/waterfall-$2.jar
+                echo "while true; do" > waterfall-$ver/start
+                echo "java -jar waterfall-$2.jar" >> waterfall-$ver/start
+                echo "echo Restarting in 15 seconds..." >> waterfall-$ver/start
+                echo "sleep 15" >> waterfall-$ver/start
+                echo "done" >> waterfall-$ver/start
+                chmod +x waterfall-$ver/start
             ;;
         esac
         exit 130
@@ -38,7 +50,11 @@ case $1 in
     vanilla|v)
         mkdir vanilla-$ver
         curl https://noahcou.github.io/fishcurl/vanilla/$2/server.jar -o vanilla-$ver/server-$2.jar
-        echo "java -Xmx4G -jar server-$2.jar" > vanilla-$ver/start
+        echo "while true; do" > vanilla-$ver/start
+        echo "java -Xmx4G -jar server-$2.jar" >> vanilla-$ver/start
+        echo "echo Restarting in 15 seconds..." >> vanilla-$ver/start
+        echo "sleep 15" >> vanilla-$ver/start
+        echo "done" >> vanilla-$ver/start
         chmod +x vanilla-$ver/start
         echo "screen -dmS vanilla-$ver ~/servers/minecraft/vanilla-$ver/start" > vanilla-$ver/screen
         chmod +x vanilla-$ver/screen
@@ -49,7 +65,11 @@ case $1 in
     paper|p)
         mkdir paper-$ver
         curl https://papermc.io/api/v1/paper/$2/latest/download -o paper-$ver/server-$2.jar
-        echo "java -Xmx8G -jar server-$2.jar" > paper-$ver/start
+        echo "while true; do" > paper-$ver/start
+        echo "java -Xmx8G -jar server-$2.jar" >> paper-$ver/start
+        echo "echo Restarting in 15 seconds..." >> paper-$ver/start
+        echo "sleep 15" >> paper-$ver/start
+        echo "done" >> paper-$ver/start
         chmod +x paper-$ver/start
         echo "screen -dmS paper-$ver ~/servers/minecraft/paper-$ver/start" > paper-$ver/screen
         chmod +x paper-$ver/screen
@@ -60,7 +80,11 @@ case $1 in
     waterfall|w)
         mkdir waterfall-$ver
         curl https://papermc.io/api/v1/waterfall/$2/latest/download -o waterfall-$ver/waterfall-$2.jar
-        echo "java -jar waterfall-$2.jar" > waterfall-$ver/start
+        echo "while true; do" > waterfall-$ver/start
+        echo "java -jar waterfall-$2.jar" >> waterfall-$ver/start
+        echo "echo Restarting in 15 seconds..." >> waterfall-$ver/start
+        echo "sleep 15" >> waterfall-$ver/start
+        echo "done" >> waterfall-$ver/start
         chmod +x waterfall-$ver/start
         echo "screen -dmS waterfall-$ver ~/servers/minecraft/waterfall-$ver/start" > waterfall-$ver/screen
         chmod +x waterfall-$ver/screen
